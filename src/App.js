49167c13770e27fe13b201f00d48b9b5dff30974
nani6129 <nani.k7396@gmail.com>
2026-04-09 12:20:47 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import Navbar from "./components/Navbar";
 import BookList from "./components/BookList";
 import AddBook from "./components/AddBook";
